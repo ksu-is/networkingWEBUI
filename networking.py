5379d29202045b1bfae24860ffb5f1e518fe48f0
hopps-daniel <77027418+hopps-daniel@users.noreply.github.com>
2021-04-05 17:51:58 -0400

    Daily work on website
    Still cannot get the websites to link properly

--- networking.py
+++ networking.py
@@ -1,28 +1,24 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-	return render_template("index.html")
-
-@app.route("/dnslookup")
+@app.route('/dnslookup')
 def dnslookup():
 	return render_template("dnslookup.html")
 
-@app.route("/hostlist")
+@app.route('/hostlist')
 def hostlist():
 	return render_template("hostlist.html")
 
-@app.route("/pinghost")
+@app.route('/pinghost')
 def pinghost():
 	return render_template("pinghost.html")
 
-@app.route("/scannetwork")
+@app.route('/scannetwork')
 def scannetwork():
 	return render_template("scannetwork.html")
 
-@app.route("/tcpdump")
+@app.route('/tcpdump')
 def tcpdump():
 	return render_template("tcpdump.html")
 
@@ -32,5 +28,4 @@ def traceroute():
 
 if __name__ == "__main__":
 	app.run(debug=True)
-
 
