308b072bddffb15048b1d51a858a417474a596a1
hopps-daniel <77027418+hopps-daniel@users.noreply.github.com>
2021-04-05 20:32:11 -0400

    Changes 4.5.21

--- networking.py
+++ networking.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+	return render_template("index.html")
+
 @app.route('/dnslookup')
 def dnslookup():
 	return render_template("dnslookup.html")
